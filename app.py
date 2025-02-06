206bf2c65b6605ee7bfbcfb486db39a03ddc169f
hyosim1996 <hyosim9300@gmail.com>
2025-02-06 17:32:35 +0900

    add

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins CI/CD with Kubernetes!!!"
+    return "Hello from Jenkins CI/CD with Kubernetes!"
 
 @app.route('/health')
 def health():
